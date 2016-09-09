6e0a67df5e1e03b0049da77ca6465dab3493b452
nwshane <django09@gmail.com>
2016-09-09 15:44:27 +0400

    Added time elapsed for budget uploader.

--- lib/budget_uploader/budget_uploader.rb
+++ lib/budget_uploader/budget_uploader.rb
@@ -4,9 +4,12 @@ require_relative 'monthly_budget_sheet/monthly_budget_sheet'
 class BudgetUploader
 
   def initialize
+    @start_time = Time.now
   end
 
   def upload_folder(folder)
+
+    puts "\nBEGIN: Budget Uploader\n\n"
     puts "Uploading all budget data from files in #{folder} to database"
 
     monthly_sheet_paths = Dir.glob(Pathname.new(folder).join('ShesBiu-*.xlsx'))
@@ -14,6 +17,14 @@ class BudgetUploader
       monthly_sheet = MonthlyBudgetSheet.new(monthly_sheet_path)
       monthly_sheet.save_data
     end
+
+    puts "\nEND: Budget Uploader"
+    puts "Time elapsed: #{elapsed_since_start.to_s}"
   end
 
+  def elapsed_since_start
+    Time.at(Time.now - start_time).utc.strftime("%H:%M:%S")
+  end
+
+  attr_reader :start_time
 end
