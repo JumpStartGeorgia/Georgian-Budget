8b322a443e2d3e1b3d126756ac469ed969e2d013
nwshane <django09@gmail.com>
2016-09-09 14:22:56 +0400

    Added with_most_recent_names scope to nameable concern; tweaked name method to sort by start date in memory, instead of adding another query with an order scope.

--- app/models/concerns/nameable.rb
+++ app/models/concerns/nameable.rb
@@ -6,13 +6,17 @@ module Nameable
   end
 
   def name
-    names.order('start_date').last.text
+    names.sort_by(&:start_date).last.text
   end
 
   module ClassMethods
     def find_by_name(name)
       joins(names: :translations)
       .where('name_translations.text = ?', name)
+    end
+
+    def with_most_recent_names
+      includes(names: :translations)
     end
   end
 end
