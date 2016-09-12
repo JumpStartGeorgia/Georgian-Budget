7c1bf47380f14b86c29d78aa5055a4f8a42c590d
nwshane <django09@gmail.com>
2016-09-12 14:05:37 +0400

    Changed code order of budget uploader spec.

--- spec/lib/budget_uploader/budget_uploader_spec.rb
+++ spec/lib/budget_uploader/budget_uploader_spec.rb
@@ -7,22 +7,34 @@ RSpec.describe 'BudgetUploader' do
       # setup
       I18n.locale = 'ka'
       test_budget_files_dir = Rails.root.join('spec', 'test_data', 'budget_files').to_s
+
+      # exercise
       uploader = BudgetUploader.new
       uploader.upload_folder(test_budget_files_dir)
 
-      # exercise
-      spending_agency1 = SpendingAgency.find_by_name('საქართველოს პარლამენტი და მასთან არსებული ორგანიზაციები')
-      program1 = Program.find_by_name('საკანონმდებლო საქმიანობა')
-
-      spending_agency2 = SpendingAgency.find_by_name('საქართველოს პრეზიდენტის ადმინისტრაცია')
-      program2 = Program.find_by_name('საქართველოს ეროვნული უშიშროების საბჭოს აპარატი')
-
       # verify
-      expect(spending_agency1.length).to eq(1)
-      expect(program1.length).to eq(1)
+      spending_agency1_array = SpendingAgency.find_by_name('საქართველოს პარლამენტი და მასთან არსებული ორგანიზაციები')
+      spending_agency1 = spending_agency1_array[0]
 
-      expect(spending_agency2.length).to eq(1)
-      expect(program2.length).to eq(1)
+      expect(spending_agency1_array.length).to eq(1)
+
+      ###
+      program1_array = Program.find_by_name('საკანონმდებლო საქმიანობა')
+      program1 = program1_array[0]
+
+      expect(program1_array.length).to eq(1)
+
+      ###
+      spending_agency2_array = SpendingAgency.find_by_name('საქართველოს პრეზიდენტის ადმინისტრაცია')
+      spending_agency2 = spending_agency2_array[0]
+
+      expect(spending_agency2_array.length).to eq(1)
+
+      ###
+      program2_array = Program.find_by_name('საქართველოს ეროვნული უშიშროების საბჭოს აპარატი')
+      program2 = program2_array[0]
+
+      expect(program2_array.length).to eq(1)
     end
   end
 end
