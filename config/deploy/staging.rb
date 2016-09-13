9f084c65ee83b958aca66de9496c2474f5c26293
nwshane <django09@gmail.com>
2016-09-13 16:53:59 +0400

    Added staging deploy config.

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -1,8 +1,8 @@
 set :domain, 'alpha.jumpstart.ge'
-set :user, 'prisoners-staging'
-set :application, 'Starter-Template-Staging'
+set :user, 'budget-staging'
+set :application, 'Budget-Staging'
 # easier to use https; if you use ssh then you have to create key on server
-set :repository, 'https://github.com/JumpStartGeorgia/Starter-Template.git'
+set :repository, 'https://github.com/JumpStartGeorgia/Georgian-Budget'
 set :branch, 'master'
 set :web_url, ENV['STAGING_WEB_URL']
 set :visible_to_robots, false
