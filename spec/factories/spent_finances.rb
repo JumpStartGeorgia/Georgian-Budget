7a0f2523c1eecd3e45bfb681e84d95c87bbc4296
nwshane <django09@gmail.com>
2016-09-19 13:04:49 +0400

    Changed spent finance factory end date into sequence to make produce spent finance time periods unique.

--- spec/factories/spent_finances.rb
+++ spec/factories/spent_finances.rb
@@ -5,7 +5,11 @@ FactoryGirl.define do
     end
 
     start_date Date.new(2015, 1, 1)
-    end_date Date.new(2015, 1, 31)
+
+    sequence :end_date do |n|
+      Date.new(2015 + n, 1, 1)
+    end
+
     association :finance_spendable, factory: :program
   end
 end
