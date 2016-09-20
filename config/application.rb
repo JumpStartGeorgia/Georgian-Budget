c154dfc73bdbc98ac669524165df8e42e68c9763
nwshane <django09@gmail.com>
2016-09-20 12:57:49 +0400

    Renamed app from StarterTemplate to GeorgianBudget.

--- config/application.rb
+++ config/application.rb
@@ -15,7 +15,7 @@ require 'sprockets/railtie'
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-module StarterTemplate
+module GeorgianBudget 
   # Comment necessary for rubocop
   class Application < Rails::Application
     # Settings in config/environments/* take precedence over those
