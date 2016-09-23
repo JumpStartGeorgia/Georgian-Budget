42d463cbc498d018848742f4d76699ec55d654d8
nwshane <django09@gmail.com>
2016-09-24 01:25:16 +0400

    Added version to api route.

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -1,8 +1,17 @@
 class ApiController < ApplicationController
   def main
-    response = APIResponse.new(params)
+    version = params['version']
+    if version == 'v1' do
+      response = APIResponse.new(params)
 
-    render json: response.to_json,
-           status: :ok
+      render json: response.to_json,
+             status: :ok
+    else
+      response = {
+        error: "API version \"#{version}\" does not exist"
+      }
+      render json: response.to_json,
+             status: :ok
+    end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -29,7 +29,7 @@ Rails.application.routes.draw do
     get '/about' => 'root#about'
     get '/list' => 'root#list'
 
-    get '/api' => 'api#main'
+    get '/api/:version' => 'api#main'
 
     # handles /en/fake/path/whatever
     get '*path', to: redirect("/#{I18n.default_locale}")
