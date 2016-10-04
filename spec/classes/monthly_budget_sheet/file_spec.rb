c0848947eca57310212adccb72fa7f85c87096b5
nwshane <django09@gmail.com>
2016-10-04 11:58:51 +0400

    Added monthly budget sheet file spec for september 2014.

--- spec/classes/monthly_budget_sheet/file_spec.rb
+++ spec/classes/monthly_budget_sheet/file_spec.rb
@@ -12,7 +12,7 @@ RSpec.describe MonthlyBudgetSheet::File do
       finance_spendable: total
     )
 
-    august_2014_sheet = Rails.root.join(
+    september_2014_sheet = Rails.root.join(
       'budget_files',
       'repo',
       'files',
@@ -21,10 +21,37 @@ RSpec.describe MonthlyBudgetSheet::File do
       'monthly_spreadsheet-08.2014.xlsx'
     ).to_s
 
-    MonthlyBudgetSheet::File.new(august_2014_sheet).save_data
+    MonthlyBudgetSheet::File.new(september_2014_sheet).save_data
 
     total.reload
 
     expect(total.spent_finances.last.amount.to_f).to eq(637285988.22)
+  end
+
+  it 'gets the right values for September, 2014' do
+    total = FactoryGirl.create(:total)
+
+    FactoryGirl.create(
+      :spent_finance,
+      amount: 5352006286.52,
+      start_date: Date.new(2014, 8, 1),
+      end_date: Date.new(2014, 8, 1).end_of_month,
+      finance_spendable: total
+    )
+
+    september_2014_sheet = Rails.root.join(
+      'budget_files',
+      'repo',
+      'files',
+      'monthly_spreadsheets',
+      '2014',
+      'monthly_spreadsheet-09.2014.xlsx'
+    ).to_s
+
+    MonthlyBudgetSheet::File.new(september_2014_sheet).save_data
+
+    total.reload
+
+    expect(total.spent_finances.last.amount.to_f).to eq(830795862.26)
   end
 end
