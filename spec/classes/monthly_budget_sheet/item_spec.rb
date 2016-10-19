c40ce7663f4f59d2d5491c1dce6da716fbfe94fd
nwshane <django09@gmail.com>
2016-10-19 13:01:29 +0400

    Refactored monthly budget sheet item_spec.

--- spec/classes/monthly_budget_sheet/item_spec.rb
+++ spec/classes/monthly_budget_sheet/item_spec.rb
@@ -1,49 +1,69 @@
 require 'rails_helper'
 
 describe MonthlyBudgetSheet::Item do
-  let(:quarter2) { Quarter.for_date(Date.new(2015, 4, 1)) }
-  let(:february) { Month.for_date(Date.new(2015, 2, 1)) }
+  let(:quarter1_2015) { Quarter.for_date(Date.new(2015, 1, 1)) }
+  let(:quarter2_2015) { Quarter.for_date(Date.new(2015, 4, 1)) }
+
+  let(:january_2015) { Month.for_date(Date.new(2015, 1, 1)) }
+  let(:february_2015) { Month.for_date(Date.new(2015, 2, 1)) }
 
   describe '#save' do
     context 'when code matches previously saved item' do
       context "and name matches previously saved item's most recent name" do
-        it 'saves planned_finance to previously saved item' do
-          code = '01 83'
-          name = 'Program name!'
+        let(:code) { '01 83' }
 
-          previously_saved_item = FactoryGirl.create(
+        let(:previously_saved_item) do
+          item = FactoryGirl.create(
             :program,
             code: code
           )
 
-          Name.create(text: name, nameable: previously_saved_item)
-
-          previously_saved_item.add_planned_finance(
-            time_period: Quarter.for_date(Date.new(2015, 1, 1)),
-            announce_date: Date.new(2015, 1, 1),
-            amount: 100
+          FactoryGirl.create(
+            :name,
+            text: 'Program name!',
+            nameable: item
           )
 
+          item
+        end
+
+        let(:header_row) do
           header_row = instance_double(MonthlyBudgetSheet::Row, 'header_row')
           allow(header_row).to receive(:is_header?).and_return(true)
           allow(header_row).to receive(:is_totals_row?).and_return(false)
           allow(header_row).to receive(:code).and_return('01 83')
           allow(header_row).to receive(:name).and_return('Program name!')
 
+          header_row
+        end
+
+        let(:totals_row) do
           totals_row = instance_double(MonthlyBudgetSheet::Row, 'totals_row')
           allow(totals_row).to receive(:is_totals_row?).and_return(true)
           allow(totals_row).to receive(:spent_finance).and_return(300)
           allow(totals_row).to receive(:planned_finance).and_return(300)
 
-          rows = [
+          totals_row
+        end
+
+        let(:rows) do
+          [
             header_row,
             totals_row
           ]
+        end
+
+        it 'saves planned_finance to previously saved item' do
+          previously_saved_item.add_planned_finance(
+            time_period: quarter1_2015,
+            announce_date: Date.new(2015, 1, 1),
+            amount: 100
+          )
 
           new_budget_item = MonthlyBudgetSheet::Item.new(
             rows,
-            quarter2.start_date,
-            quarter2.end_date
+            quarter2_2015.start_date,
+            quarter2_2015.end_date
           )
 
           new_budget_item.save
@@ -53,45 +73,17 @@ describe MonthlyBudgetSheet::Item do
         end
 
         it 'saves spent_finance to previously saved item' do
-          code = '01 83'
-          name = 'Program name!'
-
-          previously_saved_item = FactoryGirl.create(
-            :program,
-            code: code
-          )
-
-          Name.create(text: name, nameable: previously_saved_item)
-
-          january = Month.for_date(Date.new(2015, 1, 1))
-
           FactoryGirl.create(
             :spent_finance,
-            time_period: january,
+            time_period: january_2015,
             amount: 100,
             finance_spendable: previously_saved_item
           )
 
-          header_row = instance_double(MonthlyBudgetSheet::Row, 'header_row')
-          allow(header_row).to receive(:is_header?).and_return(true)
-          allow(header_row).to receive(:is_totals_row?).and_return(false)
-          allow(header_row).to receive(:code).and_return('01 83')
-          allow(header_row).to receive(:name).and_return('Program name!')
-
-          totals_row = instance_double(MonthlyBudgetSheet::Row, 'totals_row')
-          allow(totals_row).to receive(:is_totals_row?).and_return(true)
-          allow(totals_row).to receive(:spent_finance).and_return(300)
-          allow(totals_row).to receive(:planned_finance).and_return(300)
-
-          rows = [
-            header_row,
-            totals_row
-          ]
-
           new_budget_item = MonthlyBudgetSheet::Item.new(
             rows,
-            february.start_date,
-            february.end_date
+            february_2015.start_date,
+            february_2015.end_date
           )
 
           new_budget_item.save
