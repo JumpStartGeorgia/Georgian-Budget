1ee7bd719eb3dc646b6285df69591e061e51f9f4
nwshane <django09@gmail.com>
2016-10-27 21:03:25 +0400

    Refactored priority#update_planned_finances.

--- app/models/priority.rb
+++ app/models/priority.rb
@@ -42,20 +42,27 @@ class Priority < ApplicationRecord
     ).where(finance_plannable: programs)
     .group(:start_date, :end_date, :announce_date)
 
-    planned_finance_time_period_announce_dates.each do |dates_combo|
-      program_planned_finances = PlannedFinance
-      .select('DISTINCT ON (finance_plannable_type, finance_plannable_id, start_date, end_date) *')
-      .where(finance_plannable: programs)
-      .with_time_period(dates_combo.time_period)
-      .where('announce_date <= ?', dates_combo.announce_date)
-      .order(:finance_plannable_type, :finance_plannable_id, :start_date, :end_date, announce_date: :desc)
-
-      amount = PlannedFinance.find(program_planned_finances.map(&:id)).map(&:amount).sum
-
-      add_planned_finance(
-        time_period: dates_combo.time_period,
-        announce_date: dates_combo.announce_date,
-        amount: amount)
+    planned_finance_time_period_announce_dates.each do |new_planned_finance_dates|
+      create_planned_finance_with_dates(new_planned_finance_dates)
     end
+  end
+
+  def create_planned_finance_with_dates(dates)
+    add_planned_finance(
+      time_period: dates.time_period,
+      announce_date: dates.announce_date,
+      amount: planned_finance_amount_for_dates(dates))
+  end
+
+  def planned_finance_amount_for_dates(dates)
+    program_planned_finance_ids = PlannedFinance
+    .select('DISTINCT ON (finance_plannable_type, finance_plannable_id, start_date, end_date) id')
+    .where(finance_plannable: programs)
+    .with_time_period(dates.time_period)
+    .where('announce_date <= ?', dates.announce_date)
+    .order(:finance_plannable_type, :finance_plannable_id, :start_date, :end_date, announce_date: :desc)
+    .map(&:id)
+
+    amount = PlannedFinance.find(program_planned_finance_ids).map(&:amount).sum
   end
 end
