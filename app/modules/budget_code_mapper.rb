c4e4f37c8e3d75a15343a680629defae43c68ab9
nwshane <django09@gmail.com>
2016-11-07 17:21:50 +0400

    Tweaked BudgetCodeMapper.

--- app/modules/budget_code_mapper.rb
+++ app/modules/budget_code_mapper.rb
@@ -1,13 +1,17 @@
 module BudgetCodeMapper
   def self.class_for_code(code)
+    code_split = code.split(' ')
+
     if code == '00'
       return Total
-    elsif code.length == 2
+    elsif code_split.length == 1
       return Priority
-    elsif code.length == 5 && code.split(//).last(2).join == '00'
+    elsif code_split.length == 2 && code_split.last == '00'
       return SpendingAgency
-    elsif code.length >= 5 && code.length % 3 == 2
+    elsif code_split.length >= 2
       return Program
+    else
+      raise "Could not determine the class for code #{code}"
     end
   end
 end
