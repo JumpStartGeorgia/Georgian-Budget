85a51371c5b4f1e737c5ffbc51797376b91b6d44
nwshane <django09@gmail.com>
2016-11-16 16:33:14 +0400

    Split up budget files specs and added specs to check that priority perma ids are being saved.

---
+++ spec/lib/budget_files/english_translations_list_spec.rb
@@ -0,0 +1,75 @@
+require 'rails_helper'
+require Rails.root.join('lib', 'budget_uploader', 'budget_files').to_s
+
+RSpec.describe 'BudgetFiles' do
+  describe '#upload' do
+    before :example do
+      I18n.locale = 'ka'
+    end
+    
+    context 'with english translations list' do
+      before :context do
+        start_date_2015_jan_1 = Date.new(2015, 1, 1)
+
+        @priority = FactoryGirl.create(
+          :priority
+        ).add_name(
+          text_ka: 'უკატეგორიო',
+          start_date: start_date_2015_jan_1
+        )
+
+        @spending_agency = FactoryGirl.create(
+          :spending_agency
+        ).add_code(
+          number: '35 00',
+          start_date: start_date_2015_jan_1
+        ).add_name(
+          text_ka: 'საქართველოს შრომის, ჯანმრთელობისა და სოციალური დაცვის სამინისტრო',
+          start_date: start_date_2015_jan_1
+        )
+
+        @program = FactoryGirl.create(
+          :program
+        ).add_code(
+          number: '23 01',
+          start_date: start_date_2015_jan_1
+        ).add_name(
+          text_ka: 'სახელმწიფო ფინანსების მართვა',
+          start_date: start_date_2015_jan_1
+        )
+
+        @long_dash_agency = FactoryGirl.create(
+          :spending_agency
+        ).add_name(
+          text_ka: 'სახელმწიფო რწმუნებულის – გუბერნატორის ადმინისტრაცია აბაშის, ზუგდიდის, მარტვილის, მესტიის, სენაკის, ჩხოროწყუს, წალენჯიხის, ხობის მუნიციპალიტეტებსა და თვითმმართველ ქალაქ ფოთში',
+          start_date: start_date_2015_jan_1
+        )
+
+        BudgetFiles.new(
+          budget_item_translations: BudgetFiles.english_translations_file
+        ).upload
+      end
+
+      after :context do
+        [Program, SpendingAgency, Priority, Total].each(&:destroy_all)
+      end
+
+      it 'saves English translation of priority' do
+        expect(@priority.name_en).to eq('Uncategorized')
+      end
+
+      it 'saves English translation of spending agency' do
+        expect(@spending_agency.name_en).to eq(
+          'Ministry Of Labour Health and Social Affairs Georgia')
+      end
+
+      it 'saves English translation of program' do
+        expect(@program.name_en).to eq('Public Funds Management')
+      end
+
+      it 'saves English translation of program with long dash that is listed in CSV as -' do
+        expect(@long_dash_agency.name_en).to eq('Administration of the State Representative – Governor in ABASHA, ZUGDIDI, MARTVILI, MESTIA, SENAKI, CHKHOROTSKU, TSALENJIKHA, KHOBI Municipalities and the Local Self-Governing City of POTI')
+      end
+    end
+  end
+end
---
+++ spec/lib/budget_files/priorities_list_spec.rb
@@ -0,0 +1,166 @@
+require 'rails_helper'
+require Rails.root.join('lib', 'budget_uploader', 'budget_files').to_s
+
+RSpec.describe 'BudgetFiles' do
+  describe '#upload' do
+    before :example do
+      I18n.locale = 'ka'
+    end
+
+    context 'with priorities list and priority associations list' do
+      before :context do
+        @january_2012 = Month.for_date(Date.new(2012, 1, 1))
+        @quarter1_2012 = Quarter.for_date(Date.new(2012, 1, 1))
+        # Setup parliament agency
+        @parliament = SpendingAgency.create(code: '01 00')
+        @parliament.add_name(
+          text_ka: 'საქართველოს პარლამენტი და მასთან არსებული ორგანიზაციები',
+          start_date: Date.new(2012, 1, 1)
+        )
+
+        # Setup audit regulation program
+        @audit_regulation_program = Program.create(code: '01 02')
+        @audit_regulation_program.add_name(
+          text_ka: 'აუდიტორული საქმიანობის სახელმწიფო რეგულირება',
+          start_date: Date.new(2012, 1, 1)
+        )
+
+        # Setup library program
+        @library_program = Program.create(code: '01 02')
+        @library_program.add_name(
+          text_ka: 'საბიბლიოთეკო საქმიანობა',
+          start_date: Date.new(2013, 1, 1)
+        )
+
+        @library_program.add_spent_finance(
+          time_period: @january_2012,
+          amount: 30
+        )
+
+        @library_program.add_planned_finance(
+          time_period: @quarter1_2012,
+          announce_date: @quarter1_2012.start_date,
+          amount: 300
+        )
+
+        @financier_qualifications_program = Program.create(code: '23 05')
+        .add_name(
+          text_ka: 'საფინანსო სექტორში დასაქმებულთა კვალიფიკაციის ამაღლება',
+          start_date: Date.new(2012, 1, 1)
+        ).add_spent_finance(
+          time_period: @january_2012,
+          amount: 70
+        )
+
+        @financier_qualifications_program.add_planned_finance(
+          time_period: @quarter1_2012,
+          announce_date: @quarter1_2012.start_date,
+          amount: 200
+        )
+
+        # Exercise
+        BudgetFiles.new(
+          priorities_list: BudgetFiles.priorities_list,
+          priority_associations_list: BudgetFiles.priority_associations_list
+        ).upload
+      end
+
+      after :context do
+        [Program, SpendingAgency, Priority, Total].each(&:destroy_all)
+      end
+
+      let(:uncategorized_priority) do
+        Priority.find_by_name('უკატეგორიო')[0]
+      end
+
+      let(:economic_stability_priority) do
+        Priority.find_by_name(
+          'მაკროეკონომიკური სტაბილურობა და საინვესტიციო გარემოს გაუმჯობესება'
+        )[0]
+      end
+
+      let(:education_priority) do
+        Priority.find_by_name(
+          'განათლება, მეცნიერება და პროფესიული მომზადება'
+        )[0]
+      end
+
+      it 'creates uncategorized priority' do
+        expect(uncategorized_priority).to_not eq(nil)
+
+        expect(uncategorized_priority.recent_name_object.start_date)
+        .to eq(Date.new(2012, 1, 1))
+      end
+
+      it 'creates perma_id for uncategorized priority' do
+        expect(uncategorized_priority.perma_id).to_not eq(nil)
+      end
+
+      it 'assigns parliament to uncategorized priority' do
+        @parliament.reload
+        expect(@parliament.priority).to eq(uncategorized_priority)
+      end
+
+      it 'creates economic stability priority' do
+        expect(economic_stability_priority).to_not eq(nil)
+
+        expect(economic_stability_priority.recent_name_object.start_date)
+        .to eq(Date.new(2012, 1, 1))
+      end
+
+      it 'creates perma_id for economic stability priority' do
+        expect(economic_stability_priority.perma_id).to_not eq(nil)
+      end
+
+      it 'assigns audit regulation program to economic stability priority' do
+        @audit_regulation_program.reload
+        expect(@audit_regulation_program.priority).to eq(economic_stability_priority)
+      end
+
+      it 'creates education priority' do
+        expect(education_priority).to_not eq(nil)
+
+        expect(education_priority.recent_name_object.start_date)
+        .to eq(Date.new(2012, 1, 1))
+      end
+
+      it 'creates perma_id for education priority' do
+        expect(education_priority.perma_id).to_not eq(nil)
+      end
+
+      it "sets education priority's monthly spent finances" do
+        monthly_spent_finances = education_priority.spent_finances.monthly
+
+        expect(monthly_spent_finances.length).to eq(1)
+        expect(monthly_spent_finances[0].amount).to eq(100)
+        expect(monthly_spent_finances[0].time_period).to eq(@january_2012)
+      end
+
+      it "sets education priority's quarterly spent finances" do
+        quarterly_spent_finances = education_priority.spent_finances.quarterly
+
+        expect(quarterly_spent_finances.length).to eq(1)
+        expect(quarterly_spent_finances[0].amount).to eq(100)
+        expect(quarterly_spent_finances[0].time_period).to eq(
+          Quarter.for_date(Date.new(2012, 1, 1)))
+      end
+
+      it "sets education priority's planned finances" do
+        planned_finances = education_priority.planned_finances
+
+        expect(planned_finances[0].amount).to eq(500)
+        expect(planned_finances[0].time_period).to eq(@quarter1_2012)
+      end
+
+      it 'sets priority of library program' do
+        @library_program.reload
+        expect(@library_program.priority).to eq(education_priority)
+      end
+
+      it 'assigns financier_qualifications_program to education priority' do
+        @financier_qualifications_program.reload
+        expect(@financier_qualifications_program.priority).to eq(education_priority)
+      end
+    end
+  end
+end
