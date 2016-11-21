d3da7ec8bd1a5ca483fca2839a69b3706aa317e5
nwshane <django09@gmail.com>
2016-11-21 13:17:55 +0400

    Changed item overlap guard to only check overlap of official amounts.

--- app/classes/item_overlap_guard.rb
+++ app/classes/item_overlap_guard.rb
@@ -16,14 +16,14 @@ class ItemOverlapGuard
   private
 
   def check_data_with_period_type(time_period_type)
-    return false if item1.spent_finances.send(time_period_type).blank?
-    return false if item2.spent_finances.send(time_period_type).blank?
+    return false if item1.spent_finances.official.send(time_period_type).blank?
+    return false if item2.spent_finances.official.send(time_period_type).blank?
 
-    item1_start = item1.spent_finances.send(time_period_type).minimum(:start_date)
-    item1_end = item1.spent_finances.send(time_period_type).maximum(:end_date)
+    item1_start = item1.spent_finances.official.send(time_period_type).minimum(:start_date)
+    item1_end = item1.spent_finances.official.send(time_period_type).maximum(:end_date)
 
-    item2_start = item2.spent_finances.send(time_period_type).minimum(:start_date)
-    item2_end = item2.spent_finances.send(time_period_type).maximum(:end_date)
+    item2_start = item2.spent_finances.official.send(time_period_type).minimum(:start_date)
+    item2_end = item2.spent_finances.official.send(time_period_type).maximum(:end_date)
 
     return true if item1_start <= item2_end && item1_end >= item2_start
     return true if item1_end >= item2_start && item1_start <= item2_end
