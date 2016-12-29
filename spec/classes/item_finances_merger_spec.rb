04efb62f6c313d01f6aaa50707631f74cbed7f5f
nwshane <django09@gmail.com>
2016-12-29 17:31:50 +0400

    Refactored ItemFinancesMerger.

--- spec/classes/item_finances_merger_spec.rb
+++ spec/classes/item_finances_merger_spec.rb
@@ -2,14 +2,15 @@ require 'rails_helper'
 
 RSpec.describe ItemFinancesMerger do
   let(:receiver) { create(:program) }
-  let(:do_merge_finances!) { ItemFinancesMerger.new(receiver, finances).merge }
+  let(:giver) { create(:program) }
+  let(:do_merge_finances!) { ItemFinancesMerger.new(receiver, giver, finance_model).merge }
 
   context '' do
-    let(:program) do
-      create(:program, all_planned_finances: create_list(:planned_finance, 4))
-    end
+    let(:finance_model) { PlannedFinance }
 
-    let(:finances) { program.all_planned_finances }
+    before do
+      create_list(:planned_finance, 4, finance_plannable: giver)
+    end
 
     it 'moves all finances to receiver' do
       do_merge_finances!
