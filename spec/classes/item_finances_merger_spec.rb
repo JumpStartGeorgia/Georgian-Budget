91cbb92fbe6f3004560f3a5bb633491860647411
nwshane <django09@gmail.com>
2016-12-30 08:23:49 +0400

    Added more specs to item finances merger.

--- spec/classes/item_finances_merger_spec.rb
+++ spec/classes/item_finances_merger_spec.rb
@@ -3,25 +3,77 @@ require 'rails_helper'
 RSpec.describe ItemFinancesMerger do
   let(:receiver) { create(:program) }
   let(:giver) { create(:program) }
-  let(:do_merge_finances!) { ItemFinancesMerger.new(receiver, giver, finance_model).merge }
+  let(:do_merge_planned_finances!) do
+    ItemFinancesMerger.new(receiver, giver, PlannedFinance).merge
+  end
+
+  let(:do_merge_spent_finances!) do
+    ItemFinancesMerger.new(receiver, giver, SpentFinance).merge
+  end
 
   context '' do
-    let(:finance_model) { PlannedFinance }
-
     before do
+      create_list(:planned_finance, 2, finance_plannable: receiver)
       create_list(:planned_finance, 4, finance_plannable: giver)
     end
 
-    it 'moves all finances to receiver' do
-      do_merge_finances!
+    it 'moves all planned finances to receiver' do
+      do_merge_planned_finances!
 
-      expect(receiver.all_planned_finances.count).to eq(4)
+      expect(receiver.all_planned_finances.count).to eq(6)
     end
   end
 
-  context 'when giver has two plans preceded by receiver plan' do
-    let(:finance_model) { PlannedFinance }
+  context '' do
+    before do
+      create_list(:spent_finance, 2, finance_spendable: receiver)
+      create_list(:spent_finance, 4, finance_spendable: giver)
+    end
 
+    it 'moves all spent finances to receiver' do
+      do_merge_spent_finances!
+
+      expect(receiver.all_spent_finances.count).to eq(6)
+    end
+  end
+
+  context '' do
+    let!(:receiver_plan_q1) do
+      create(:planned_finance,
+        finance_plannable: receiver,
+        time_period_obj: Quarter.for_date(Date.new(2013, 1, 1)),
+        primary: true
+      )
+    end
+
+    let!(:giver_plan_q2) do
+      create(:planned_finance,
+        finance_plannable: giver,
+        time_period_obj: Quarter.for_date(Date.new(2013, 4, 1))
+      )
+    end
+
+    it 'deaccumulates quarterly planned finances' do
+      original_amount = giver_plan_q2.amount
+      do_merge_planned_finances!
+
+      expect(giver_plan_q2.reload.amount).to eq(
+        original_amount - receiver_plan_q1.amount
+      )
+    end
+  end
+
+  it 'does not deaccumulate monthly planned finances'
+  it 'does not deaccumulate yearly planned finances'
+
+  it 'deaccumulates monthly spent finances'
+  it 'does not deaccumulate quarterly spent finances'
+  it 'does not deaccumulate yearly spent finances'
+
+  it 'deaccumulates receiver quarterly planned finances'
+  it 'deaccumulates receiver monthly spent finances'
+
+  context 'when giver has two plans preceded by receiver plan' do
     let!(:receiver_plan_q1) do
       create(:planned_finance,
         finance_plannable: receiver,
@@ -48,7 +100,7 @@ RSpec.describe ItemFinancesMerger do
 
     it 'deaccumulates giver primary plan amount' do
       original_amount = giver_plan_primary.amount
-      do_merge_finances!
+      do_merge_planned_finances!
 
       expect(giver_plan_primary.reload.amount).to eq(
         original_amount - receiver_plan_q1.amount
@@ -57,7 +109,7 @@ RSpec.describe ItemFinancesMerger do
 
     it 'deaccumulates giver non primary plan amount' do
       original_amount = giver_plan_non_primary.amount
-      do_merge_finances!
+      do_merge_planned_finances!
 
       expect(giver_plan_non_primary.reload.amount).to eq(
         original_amount - receiver_plan_q1.amount
