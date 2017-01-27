e7f472ec2cba731dc1caed7fad8cb2c63495d8bf
nwshane <django09@gmail.com>
2017-01-27 15:32:58 +0400

    Added error rendering if csv does not exist.

--- app/controllers/csv_controller.rb
+++ app/controllers/csv_controller.rb
@@ -3,6 +3,11 @@ class CsvController < ApplicationController
   def primary_finances
     locale = params[:locale]
     filename = "primary_finances_#{locale}.zip"
-    send_file Rails.root.join('public', 'system', 'csv', filename)
+    filepath = Rails.root.join('public', 'system', 'csv', filename)
+    if (File.exist?(filepath))
+      send_file filepath
+    else
+      render json: 'File does not exist. Please notify JumpStart Georgia of this issue.', status: 404
+    end
   end
 end
