c864f04f7812d2083846b99e9c85862ffa1fd5e3
nwshane <django09@gmail.com>
2017-03-20 15:39:16 +0400

    Simplified last updated date controller.

--- app/controllers/api/v1/last_updated_date_controller.rb
+++ app/controllers/api/v1/last_updated_date_controller.rb
@@ -2,7 +2,7 @@ module Api
   module V1
     class LastUpdatedDateController < ApplicationController
       def index
-        last_updated_date = Date.strptime(SpentFinance.maximum(:created_at).to_s)
+        last_updated_date = SpentFinance.maximum(:created_at).to_date
 
         if (last_updated_date)
           render json: { last_updated_date: last_updated_date }, status: 200
