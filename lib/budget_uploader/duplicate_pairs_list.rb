568e0a8e8fc7ce579c4f154b40360bcb606b5c87
nwshane <django09@gmail.com>
2017-03-20 10:46:01 +0400

    Removed find_unmergeable_pairs rake task.

--- lib/budget_uploader/duplicate_pairs_list.rb
+++ lib/budget_uploader/duplicate_pairs_list.rb
@@ -14,47 +14,7 @@ class DuplicatePairsList
     end
   end
 
-  def output_unmergeable_pairs
-    require('csv')
-    CSV.read(file_path).each_with_index do |row, index|
-      next if index === 0
-
-      output_if_unmergeable(row, index)
-    end
-  end
-
   private
-
-  def output_if_unmergeable(row, index)
-    item1 = item1(row)
-    item2 = item2(row)
-    pair = pair(row)
-
-    messages = []
-    if item1.blank?
-      messages << 'Cannot find item1'
-    end
-
-    if item2.blank?
-      messages << 'Cannot find item2'
-    end
-
-    if pair.blank?
-      messages << 'Cannot find pair'
-    end
-
-    if merge_items?(row)
-      begin
-        MergeGuard.new(item1, item2).enforce_merge_okay
-      rescue MergeImpossibleError => e
-        messages << e
-      end
-    end
-
-    return if messages.empty?
-    puts "\n Row #{index + 1} (Codes #{row[5]}, #{row[6]}) problems:"
-    puts messages.join("\n")
-  end
 
   def process_row(row)
     return if item1(row).blank?
