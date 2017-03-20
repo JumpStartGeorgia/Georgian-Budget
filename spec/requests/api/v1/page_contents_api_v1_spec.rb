ab550fa3e8e1a755ae479e0a05755d835941b066
nwshane <django09@gmail.com>
2017-03-20 15:10:32 +0400

    Added assertion to page content api v1 request spec.

--- spec/requests/api/v1/page_contents_api_v1_spec.rb
+++ spec/requests/api/v1/page_contents_api_v1_spec.rb
@@ -3,13 +3,15 @@ require('rails_helper')
 RSpec.describe 'Page Contents API V1' do
   context 'when requesting about page content' do
     let!(:about_page_content) do
-      create(:page_content, name: 'about')
+      create(:page_content, name: 'about', content: 'Hellooo bla bla')
     end
 
     it 'returns about page content' do
       get '/en/v1/page_contents/about'
 
       json = JSON.parse(response.body)
+
+      expect(json['content']).to eq(about_page_content.content)
     end
   end
 end
