6998b64dac55b8785b35d36dd955875ed4a5cdee
nwshane <django09@gmail.com>
2017-03-24 12:21:24 +0400

    Removed brakeman and rubocop tests.

--- spec/tools/rubocop_spec.rb
+++
@@ -1,9 +0,0 @@
-require 'json'
-
-describe 'rubocop' do
-  let!(:rubocop_json) { JSON.parse(`rubocop --format json`) }
-
-  it 'has no code style offenses' do
-    expect(rubocop_json['summary']['offense_count']).to eq(0)
-  end
-end
